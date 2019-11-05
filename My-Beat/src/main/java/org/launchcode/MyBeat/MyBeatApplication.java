1a692472d3ed92530beae63fa1501bb2face56a6
Jon Etchison <jetch5@yahoo.com>
2019-11-04 21:29:57 -0600

    initial update with gradle package

---
+++ My-Beat/src/main/java/org/launchcode/MyBeat/MyBeatApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.MyBeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBeatApplication.class, args);
+	}
+
+}
---
+++ My-Beat/src/test/java/org/launchcode/MyBeat/MyBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.MyBeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
